c00a72ba18405a29b9260625d020daab56ac793c
danangoffic <darifrahmanda@gmail.com>
2022-07-19 03:23:59 +0700

    install breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -265,7 +265,7 @@
                                     <div class="clear"></div>
                                 </div>
                                 <p>
-                                    <a href="#" class="btn btn-master btn-primary w-100 mt-3">
+                                    <a href="{{ route('checkout') }}" class="btn btn-master btn-primary w-100 mt-3">
                                         Take This Plan
                                     </a>
                                 </p>
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
 Route::get('checkout', function () {
     return view('checkout');
-});
+})->name('checkout');
 Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
